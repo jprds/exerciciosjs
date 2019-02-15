095c4fe1b6465f487ba1f7a0c5945531f725ea35
jprds <11419@aebenfica.org>
2019-02-15 09:38:59 +0000

    fking variables in a single line

--- matematica/add2.js
+++ matematica/add2.js
@@ -1,5 +1,7 @@
-var num1, num2, sum;
+var num1;
+var num2;
+var sum;
     num1 = prompt("Enter first number");
     num2 = prompt("Enter second number");
     sum = parseInt(num1) + parseInt(num2) // "+" means "add";
-    alert("Sum = " + sum)  // "+" means combine into a string;
+    alert("Sum =" + sum)  // "+" means combine into a string;
